29cb286667104e69ce7a81173c5de06b0f8ad4c5
Zafar086 <ergashevzafar1777@gmail.com>
2025-04-22 14:40:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const third = prompt("what is your name?");
+
+const one = prompt("How old are you?");
+
+const two = prompt("Where you live?");
+
+confirm("kiritgan ma'lumotlaringiz to'g'rimi?")
+
+// alert("My name is " + third + " I'm " + one + " years old " + " I am from " + two);
+alert(` My name is ${third}. I am ${one} years old. I am from ${two}`);
+
+console.log(`Name: ${third}`)
+console.log(`old: ${one}`)
+console.log(`Adress: ${two}`)
+
+
